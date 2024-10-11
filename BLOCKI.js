c975e954be67a719711efbbf618f356d0767e791
ZaXron <76252428+ZaXron@users.noreply.github.com>
2024-10-11 14:43:18 +0300

    Add files via upload

---
+++ BLOCKI.js
@@ -0,0 +1,52 @@
+// let divList = document.querySelectorAll(".block");
+
+
+// for (const item of divList) {
+//     item.style.width = "1000px";
+//     item.style.height = "180px";
+//     item.style.marginLeft = "10px";
+//     item.style.padding = "20px 0 20px 0";
+// }
+
+// let block1 = document.getElementById("block1");
+// block1.style.borderBottom = "2px solid rgba(0, 0, 0, .3)";
+
+// let block2 = document.getElementById("block2");
+// block2.style.borderBottom = "2px solid rgba(0, 0, 0, .2)";
+
+// let block3 = document.getElementById("block3");
+// block3.style.borderBottom = "2px solid rgba(0, 0, 0, .1)";
+
+// let wrap = document.querySelectorAll("wrapper");
+// for (const item of wrap) {
+//     item.style.width = "100%";
+//     item.style.alignItems = "baseline";
+//     item.style.justifyContent = "space-between";
+//     item.style.display = "flex";
+// }
+
+// let titles = document.querySelectorAll("h2");
+// for (const item of titles) {
+//     item.style.fontFamily = "Roboto";
+//     item.style.fontWeight = "600";
+//     item.style.fontSize = "24px";
+//     item.style.height = "30px";
+// }
+
+// let buttons = document.querySelectorAll("Button");
+// for (const item of buttons) {
+//     item.style.height = "25px";
+//     item.style.fontFamily = "Roboto";
+//     item.style.fontWeight = "400";
+//     item.style.fontSize = "15px";
+// }
+
+
+// let paragraphs = document.querySelectorAll("p");
+// for (const item of paragraphs) {
+//     item.style.fontFamily = "Roboto";
+//     item.style.fontWeight = "400";
+//     item.style.fontSize = "15px";
+//     item.style.height = "30px";
+//     item.style.textAlign = "justify";
+// }
